c8e59291565d72d3045f3c7397b6911fff89efb0
matebalhasi <matebalhasi@gmail.com>
2024-12-07 17:11:53 +0100

    Update Main Menu.cpp
    Added authentication. Fixed design elements

--- Main Menu.cpp
+++ Main Menu.cpp
@@ -8,85 +8,118 @@
 
 using namespace std;
 
+//press key to continue
+void pressToContinue()
+{
+#ifdef _WIN32
+	_getch();
+#else
+	struct termios oldt, newt;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	getchar();
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+#endif
+}
+
+
+
+
+//The main menu
 void showMainMenu()
 {
 	cout << "Main Menu" << endl;
 	cout << "-------------" << endl;
 	cout << "Select which user you wish to use:" << endl;
-	cout << "1. Worker \n 2. Buyer \n 3. Supplier \n 4. Admin \n 5. Exit\n \n Enter a choice (1-5) ";
+	cout << " 1. Worker \n 2. Buyer \n 3. Supplier \n 4. Admin \n 5. Exit\n \n Enter a choice (1-5) ";
 }
+//Sub menu for Admins
 void showAdminMenu()
 {
 	cout << "Admin Menu" << endl;
 	cout << "-------------" << endl;
 	cout << "Select which user you wish to use:" << endl;
-	cout << "1. Personal Data \n 2. Worker Data \n 3. Storage Data \n 4. Complaints \n 5. Items\n 6. Exit\n \n Enter a choice (1-6) ";
+	cout << " 1. Personal Data \n 2. Worker Data \n 3. Storage Data \n 4. Complaints \n 5. Items\n 6. Exit\n \n Enter a choice (1-6) ";
 }
+//Sub menu for Workers
 void showWorkerMenu()
 {
 	cout << "Worker Menu" << endl;
 	cout << "-------------" << endl;
 	cout << "Select which user you wish to use:" << endl;
-	cout << "1. Personal Data \n 2. Storage \n 3. Items \n 4. Exit \n  \n Enter a choice (1-4) ";
+	cout << " 1. Personal Data \n 2. Storage \n 3. Items \n 4. Exit \n  \n Enter a choice (1-4) ";
 }
+//Sub Menu for Buyers
 void showBuyerMenu()
 {
 	cout << "Buyer Menu" << endl;
 	cout << "-------------" << endl;
 	cout << "Select which user you wish to use:" << endl;
-	cout << "1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
+	cout << " 1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
 }
+//Sub menu for Suppliers
 void showSupplierMenu()
 {
 	cout << "Supplier Menu" << endl;
 	cout << "-------------" << endl;
 	cout << "Select which user you wish to use:" << endl;
-	cout << "1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
+	cout << " 1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
 }
 
-bool loginValidation(string ID, string password, char clearanceLevel, char menuPart)
+bool loginValidation(const string id, const string password, const char clearance_level )
 {
-	ifstream users("user.txt");
+
+	ifstream users("users.txt");
 	if(!users.is_open())
 	{
-		cerr << "There was an error with the user file \n";
+		cout << "Error! The file could not be upened! \n";
 		return false;
 	}
 
-	string line, userID, userPassword;
-	char userLevel;
 
-	while (getline(users, line))
+	string input, userID, userPassword;
+	char user_clearance_level;
+	while(getline(users, input))
 	{
-		istringstream iss(line);
-		iss >> userID >> userPassword >> userLevel;
-
-		if (userID == ID && userPassword == password)
+		istringstream iss(input);
+		iss >> userID >> userPassword >> user_clearance_level;
+		
+		if(userID == id && userPassword== password)
 		{
-			if (userLevel == clearanceLevel)
+			if (user_clearance_level == clearance_level|| user_clearance_level == '4')
 			{
-				if (userLevel == menuPart)
-				{
-					return true;
-				}
-				else
-				{
-					cerr << "Error! User clearance not valid for this menu!";
-					return false;
-				}
+				return true;
 			}
+			
 			else
 			{
-				cerr << "Error! User ID or Password is Invalid";
+				system("cls");
+				cout << "Error! User clearance level does not match!";
+				cout << "\n\nPress any key to continue.";
+				pressToContinue();
+				return false;
+					
 			}
+			
+			
 		}
-
 	}
+	system("cls");
+	cout << "Error! User Id and/or password does not match!";
+	cout << "\n\nPress any key to continue.";
+	pressToContinue();
+	return false;
+
 }
+
+
 
 
 int main()
 {
+
 	char input;
 	string id, password;
 	showMainMenu();
@@ -99,76 +132,134 @@ int main()
 
 		if (input == '1')
 		{
-				/*system("cls");
-				cout << "Enter user ID:";
-				cin >> input;
-				cout << "Enter user password:";
-				cin >> input;*/
-				//if(loginValidation())
-				
-				showWorkerMenu();
-				cin >> input;
-				while (true)
-				{
-					if (input == '4')
-					{
-						system("cls");
-						showMainMenu();
-						break;
-					}
+		
+			system("cls");
 
+			cout << "Enter userID:";
+			cin >> id;
+			cout << "\n Enter Password:";
+			cin >> password;
+			
+				
+				if(loginValidation(id,password,input))
+				{
+					system("cls");
+					showWorkerMenu();
+					cin >> input;
+					while (true)
+					{
+						if (input == '4')
+						{
+							system("cls");
+							showMainMenu();
+							break;
+						}
+
+					}
+				}else
+				{
+					system("cls");
+					showMainMenu();
 				}
+				
 
 			}
 			else if (input == '2')
 			{
+			
 				system("cls");
-				showBuyerMenu();
-				cin >> input;
-				while (true)
-				{
 
-					if (input == '4')
+				cout << "Enter userID:";
+				cin >> id;
+				cout << "\n Enter Password:";
+				cin >> password;
+
+
+				if (loginValidation(id, password, input))
+				{
+					system("cls");
+					showBuyerMenu();
+					cin >> input;
+					while (true)
 					{
-						system("cls");
-						showMainMenu();
-						break;
+
+						if (input == '4')
+						{
+							system("cls");
+							showMainMenu();
+							break;
+						}
 					}
 				}
-
+				else
+				{
+					system("cls");
+					showMainMenu();
+				}
 			}
 			else if (input == '3')
 			{
+				
 				system("cls");
-				showSupplierMenu();
-				cin >> input;
-				while (true)
-				{
-					if (input == '4')
-					{
-						system("cls");
-						showMainMenu();
-						break;
-					}
 
+				cout << " Enter userID:";
+				cin >> id;
+				cout << "\n Enter Password:";
+				cin >> password;
+				if (loginValidation(id, password, input))
+				{
+					system("cls");
+					showSupplierMenu();
+					cin >> input;
+					while (true)
+					{
+						if (input == '4')
+						{
+							system("cls");
+							showMainMenu();
+							break;
+						}
+
+					}
 				}
+				else
+				{
+					system("cls");
+					showMainMenu();
+				}
+				
 
 			}
 			else if (input == '4')
 			{
+			
 				system("cls");
-				showAdminMenu();
-				cin >> input;
-				while (true)
+				cout << " Enter userID:";
+				cin >> id;
+				cout << "\n Enter Password:";
+				if(loginValidation(id,password,input))
 				{
-					if (input == '6')
+					system("cls");
+					showAdminMenu();
+					cin >> input;
+					while (true)
 					{
-						system("cls");
-						showMainMenu();
-						break;
-					}
+						if (input == '6')
+						{
+							system("cls");
+							showMainMenu();
+							break;
+						}
 
+					}
 				}
+				else
+				{
+					system("cls");
+					showMainMenu();
+				}
+				
+				
 
 			}
 			else if (input == '5')
