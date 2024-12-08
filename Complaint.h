cc45458580692873518d113daeb9d5a688ebeb2c
Hindemith00 <91939821+Hindemith00@users.noreply.github.com>
2024-12-08 17:27:30 +0100

    Add files via upload

--- Complaint.h
+++ Complaint.h
@@ -10,24 +10,7 @@ public:
 	list<string>lista;
 	list<string>::iterator it;
 	int sorszam = 0;
-	/*
-	bool operator<(const Complaint& c) const {
-		if (it<c.it)
-			return false;
-		if (it <c.it)
-			return true;
-		return it< c.it;
-	}
 	
-	friend std::ostream& operator<<(std::ostream& s, const list<string>lista) {
-		for (auto it=lista.begin(); it < lista.end(); it++)
-		{
-			s << lista.it;
-		}
-		return s;
-	}
-	*/
-
 	void beolvas() {
 		std::ifstream input("complaints.txt");
 		string s;
@@ -72,7 +55,7 @@ public:
 		 lista.push_back(mycomplaint);
 		 ofstream of("complaints.txt");
 		 for (auto v : lista)
-			 of << v<< "\n";
+		of << v<< "\n";
 		 of.close();
 	 }
 		
