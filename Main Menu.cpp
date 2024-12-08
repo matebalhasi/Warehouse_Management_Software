10e9b348a04651e6dd3dfd2109361de21b3d9d64
matebalhasi <matebalhasi@gmail.com>
2024-12-08 16:24:01 +0100

    reworked mainmenu, added personalData
    Reworked the main menu slightly so it takes up less space.
    Added a PersonalData header file that writes out the current user's personal data. For the admin submenu they can also list out the data of every worker and admin except themselves to see their data.

--- Main Menu.cpp
+++ Main Menu.cpp
@@ -3,288 +3,270 @@
 #include <thread>
 #include <fstream>
 #include <sstream>
-
-
+#include "PersonalData.h"
 
 using namespace std;
 
-//press key to continue
-void pressToContinue()
-{
+// Press key to continue
+void pressToContinue() {
 #ifdef _WIN32
-	_getch();
+    _getch();
 #else
-	struct termios oldt, newt;
-	tcgetattr(STDIN_FILENO, &oldt);
-	newt = oldt;
-	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-	getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    struct termios oldt, newt;
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 #endif
 }
 
-
-
-
-//The main menu
-void showMainMenu()
-{
-	cout << "Main Menu" << endl;
-	cout << "-------------" << endl;
-	cout << "Select which user you wish to use:" << endl;
-	cout << " 1. Worker \n 2. Buyer \n 3. Supplier \n 4. Admin \n 5. Exit\n \n Enter a choice (1-5) ";
-}
-//Sub menu for Admins
-void showAdminMenu()
-{
-	cout << "Admin Menu" << endl;
-	cout << "-------------" << endl;
-	cout << "Select which user you wish to use:" << endl;
-	cout << " 1. Personal Data \n 2. Worker Data \n 3. Storage Data \n 4. Complaints \n 5. Items\n 6. Exit\n \n Enter a choice (1-6) ";
-}
-//Sub menu for Workers
-void showWorkerMenu()
-{
-	cout << "Worker Menu" << endl;
-	cout << "-------------" << endl;
-	cout << "Select which user you wish to use:" << endl;
-	cout << " 1. Personal Data \n 2. Storage \n 3. Items \n 4. Exit \n  \n Enter a choice (1-4) ";
-}
-//Sub Menu for Buyers
-void showBuyerMenu()
-{
-	cout << "Buyer Menu" << endl;
-	cout << "-------------" << endl;
-	cout << "Select which user you wish to use:" << endl;
-	cout << " 1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
-}
-//Sub menu for Suppliers
-void showSupplierMenu()
-{
-	cout << "Supplier Menu" << endl;
-	cout << "-------------" << endl;
-	cout << "Select which user you wish to use:" << endl;
-	cout << " 1. Storage \n 2. Add Item \n 3. Write Complaint \n 4. Exit\n \n Enter a choice (1-5) ";
+// Main menu
+void showMainMenu() {
+    cout << "Main Menu\n"
+        << "-------------\n"
+        << "Select which user you wish to use:\n"
+        << " 1. Worker\n 2. Buyer\n 3. Supplier\n 4. Admin\n 5. Exit\n"
+        << "\n Enter a choice (1-5): ";
 }
 
-bool loginValidation(const string id, const string password, const char clearance_level )
-{
+// Submenus
+void showWorkerMenu() {
+    cout << "Worker Menu\n"
+        << "-------------\n"
+        << " 1. Personal Data\n 2. Storage\n 3. Items\n 4. Exit\n"
+        << "\n Enter a choice (1-4): ";
+}
 
-	ifstream users("users.txt");
-	if(!users.is_open())
-	{
-		cout << "Error! The file could not be upened! \n";
-		return false;
-	}
+void showBuyerMenu() {
+    cout << "Buyer Menu\n"
+        << "-------------\n"
+        << " 1. Storage\n 2. Add Item\n 3. Write Complaint\n 4. Exit\n"
+        << "\n Enter a choice (1-4): ";
+}
 
+void showSupplierMenu() {
+    cout << "Supplier Menu\n"
+        << "-------------\n"
+        << " 1. Storage\n 2. Add Item\n 3. Write Complaint\n 4. Exit\n"
+        << "\n Enter a choice (1-4): ";
+}
 
-	string input, userID, userPassword;
-	char user_clearance_level;
-	while(getline(users, input))
-	{
-		istringstream iss(input);
-		iss >> userID >> userPassword >> user_clearance_level;
-		
-		if(userID == id && userPassword== password)
-		{
-			if (user_clearance_level == clearance_level|| user_clearance_level == '4')
-			{
-				return true;
-			}
-			
-			else
-			{
-				system("cls");
-				cout << "Error! User clearance level does not match!";
-				cout << "\n\nPress any key to continue.";
-				pressToContinue();
-				return false;
-					
-			}
-			
-			
-		}
-	}
-	system("cls");
-	cout << "Error! User Id and/or password does not match!";
-	cout << "\n\nPress any key to continue.";
-	pressToContinue();
-	return false;
+void showAdminMenu() {
+    cout << "Admin Menu\n"
+        << "-------------\n"
+        << " 1. Personal Data\n 2. Worker Data\n 3. Storage Data\n"
+        << " 4. Complaints\n 5. Items\n 6. Exit\n"
+        << "\n Enter a choice (1-6): ";
+}
 
+// Validate login credentials
+bool loginValidation(const string& id, const string& password, char clearance_level) {
+    ifstream users("users.txt");
+    if (!users.is_open()) {
+        cout << "Error! The file could not be opened!\n";
+        return false;
+    }
+
+    string input, userID, userPassword;
+    char user_clearance_level;
+    while (getline(users, input)) {
+        istringstream iss(input);
+        iss >> userID >> userPassword >> user_clearance_level;
+
+        if (userID == id && userPassword == password) {
+            if (user_clearance_level == clearance_level || user_clearance_level == '4') {
+                return true;
+            }
+            else {
+                system("cls");
+                cout << "Error! User clearance level does not match!\n\nPress any key to continue.";
+                pressToContinue();
+                return false;
+            }
+        }
+    }
+
+    system("cls");
+    cout << "Error! User ID and/or password do not match!\n\nPress any key to continue.";
+    pressToContinue();
+    return false;
 }
 
 
+int main() {
+    PersonalData personalData("worker_data.txt");
+    char input;
+    string id, password;
 
+    showMainMenu();
+    cin >> input;
 
-int main()
-{
+    while (true) {
+        system("cls");
+        switch (input) {
+            //worker Menu
+        case '1': { 
+            cout << "Enter userID: ";
+            cin >> id;
+            cout << "\nEnter Password: ";
+            cin >> password;
 
-	char input;
-	string id, password;
-	showMainMenu();
-	cin >> input;
-	while (true)
-	{
-		//<<
+            if (loginValidation(id, password, input)) {
+                system("cls");
+                showWorkerMenu();
+                cin >> input;
 
+                while (true) {
+                    if (input == '1') { 
+                        system("cls");
+                        cout << "Fetching Personal Data";
+                        for (int i = 0; i < 3; ++i) {
+                            this_thread::sleep_for(chrono::milliseconds(500));
+                            cout << ".";
+                        }
+                        system("cls");
+                        cout << personalData.getWorkerInfo(id) << endl;
+                        pressToContinue();
+                        system("cls");
+                        showWorkerMenu();
+                        cin >> input;
+                    }
+                    else if (input == '4') { 
+                        system("cls");
+                        showMainMenu();
+                        break;
+                    }
+                }
+            }
+            else {
+                showMainMenu();
+            }
+            break;
+        }
+                //buyer Menu
+        case '2': { 
+            cout << "Enter userID: ";
+            cin >> id;
+            cout << "\nEnter Password: ";
+            cin >> password;
 
+            if (loginValidation(id, password, input)) {
+                system("cls");
+                showBuyerMenu();
+                cin >> input;
 
-		if (input == '1')
-		{
-		
-			system("cls");
+                while (true) {
+                    if (input == '4') {
+                        system("cls");
+                        showMainMenu();
+                        break;
+                    }
+                }
+            }
+            else {
+                showMainMenu();
+            }
+            break;
+        }
+                //supplier Menu
+        case '3': {
+            cout << "Enter userID: ";
+            cin >> id;
+            cout << "\nEnter Password: ";
+            cin >> password;
 
-			cout << "Enter userID:";
-			cin >> id;
-			cout << "\n Enter Password:";
-			cin >> password;
-			
-				
-				if(loginValidation(id,password,input))
-				{
-					system("cls");
-					showWorkerMenu();
-					cin >> input;
-					while (true)
-					{
-						if (input == '4')
-						{
-							system("cls");
-							showMainMenu();
-							break;
-						}
+            if (loginValidation(id, password, input)) {
+                system("cls");
+                showSupplierMenu();
+                cin >> input;
 
-					}
-				}else
-				{
-					system("cls");
-					showMainMenu();
-				}
-				
+                while (true) {
+                    if (input == '4') {
+                        system("cls");
+                        showMainMenu();
+                        break;
+                    }
+                }
+            }
+            else {
+                showMainMenu();
+            }
+            break;
+        }
+                //admin Menu
+        case '4': { 
+            cout << "Enter userID: ";
+            cin >> id;
+            cout << "\nEnter Password: ";
+            cin >> password;
 
-			}
-			else if (input == '2')
-			{
-			
-				system("cls");
+            if (loginValidation(id, password, input)) {
+                system("cls");
+                showAdminMenu();
+                cin >> input;
 
-				cout << "Enter userID:";
-				cin >> id;
-				cout << "\n Enter Password:";
-				cin >> password;
+                while (true) {
 
+                    
+                   if(input== '1')
+                   {
+						system("cls");
+                        cout << "Fetching Personal Data";
+                        for (int i = 0; i < 3; ++i) {
+                            this_thread::sleep_for(chrono::milliseconds(500));
+                            cout << ".";
+                        }
+                        system("cls");
+                        cout << personalData.getWorkerInfo(id) << endl;
+                        pressToContinue();
+                        system("cls");
+                        showAdminMenu();
+                        cin >> input;
+                   }
 
-				if (loginValidation(id, password, input))
-				{
-					system("cls");
-					showBuyerMenu();
-					cin >> input;
-					while (true)
-					{
+                  
+                        
+                
+                   
+                    
 
-						if (input == '4')
-						{
-							system("cls");
-							showMainMenu();
-							break;
-						}
-					}
-				}
-				else
-				{
-					system("cls");
-					showMainMenu();
-				}
-			}
-			else if (input == '3')
-			{
-				
-				system("cls");
+                	if(input == '6')
+                	{
+                		system("cls");
+                        showMainMenu();
+                        break;
+                	}
+                }
+            }
+            else {
+                showMainMenu();
+            }
+            break;
+        }
 
-				cout << " Enter userID:";
-				cin >> id;
-				cout << "\n Enter Password:";
-				cin >> password;
-				if (loginValidation(id, password, input))
-				{
-					system("cls");
-					showSupplierMenu();
-					cin >> input;
-					while (true)
-					{
-						if (input == '4')
-						{
-							system("cls");
-							showMainMenu();
-							break;
-						}
+        case '5': { 
+            system("cls");
+            cout << "Exiting";
+            for (int i = 0; i < 3; ++i) {
+                this_thread::sleep_for(chrono::milliseconds(500));
+                cout << ".";
+            }
+            exit(1);
+        }
 
-					}
-				}
-				else
-				{
-					system("cls");
-					showMainMenu();
-				}
-				
+        default: {
+            cout << "Error: Invalid option. Try again.\n";
+            this_thread::sleep_for(chrono::milliseconds(1500));
+            showMainMenu();
+            cin >> input;
+            break;
+        }
+        }
+        cin >> input;
+    }
+}
 
-			}
-			else if (input == '4')
-			{
-			
-				system("cls");
-				cout << " Enter userID:";
-				cin >> id;
-				cout << "\n Enter Password:";
-				if(loginValidation(id,password,input))
-				{
-					system("cls");
-					showAdminMenu();
-					cin >> input;
-					while (true)
-					{
-						if (input == '6')
-						{
-							system("cls");
-							showMainMenu();
-							break;
-						}
-
-					}
-				}
-				else
-				{
-					system("cls");
-					showMainMenu();
-				}
-				
-				
-
-			}
-			else if (input == '5')
-			{
-				system("cls");
-				cout << "Exiting";
-				for (int i = 0; 3 > i; i++)
-				{
-					this_thread::sleep_for(chrono::milliseconds(500));
-					cout << ".";
-				}
-
-				break;
-			}
-			else
-			{
-				system("cls");
-				cout << "Error: Wrong option.";
-				this_thread::sleep_for(chrono::milliseconds(1500));
-				system("cls");
-				showMainMenu();
-				cin >> input;
-			}
-
-			cin >> input;
-
-		}
-	}
+//Ez azert van itt mert Mate billentyuzete szar
+//<<
---
+++ PersonalData.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <unordered_map>
+
+
+using namespace std;
+
+class PersonalData
+{
+private:
+	struct Worker
+	{
+		string userID;
+		string name;
+		string position;
+		string hoursWorked;
+		string PTORemaining;
+	};
+	unordered_map<std::string, Worker> workers;
+	string filePath;
+
+	
+public:
+	PersonalData(const string& filePath) {
+		loadWorkerData(filePath);
+	}
+	void saveWorkerData(const string& filePath) const {
+		ofstream file(filePath);
+		if (!file.is_open()) {
+			cout << "Error! File could not be opened for writing: " << filePath << endl;
+			return;
+		}
+
+		for (const auto& [userID, worker] : workers) {
+			file << worker.userID << " "
+				<< worker.name << " "
+				<< worker.position << " "
+				<< worker.hoursWorked << " "
+				<< worker.PTORemaining << "\n";
+		}
+		file.close();
+	}
+	void loadWorkerData(const string& filePath) {
+		ifstream file(filePath);
+		if (!file.is_open()) {
+			cout << "Error! File could not be opened: " << filePath << endl;
+			return;
+		}
+
+		string line;
+		while (getline(file, line)) {
+			istringstream iss(line);
+			Worker worker;
+			if (!(iss >> worker.userID >> worker.name >> worker.position >> worker.hoursWorked >> worker.PTORemaining)) {
+				cout << "Error! Malformed line in " << filePath << endl;
+				continue;
+			}
+			workers[worker.userID] = worker;
+		}
+		file.close();
+	}
+
+	string getWorkerInfo(string userID)const
+	{
+		auto it = workers.find(userID);
+		if (it != workers.end()) {
+			const Worker& worker = it->second;
+			ostringstream oss;
+			oss << "Name: " << worker.name
+				<< "\nPosition: " << worker.position
+				<< "\nHours Worked: " << worker.hoursWorked
+				<< "\nPTO Remaining: " << worker.PTORemaining;
+			return oss.str();
+		}
+		else {
+			return "Error: Worker not found";
+		}
+		
+	}
+	void listWorkers(const string& currentUserID) const {
+		cout << "Worker List :" << endl;
+		for (const auto& pair : workers) {
+			const auto& worker = pair.second;
+			const auto& userID = pair.first;
+
+			if (userID != currentUserID) {
+				cout << "UserID: " << userID
+					<< ", Name: " << worker.name
+					<< ", Position: " << worker.position
+					<< ", Hours Worked: " << worker.hoursWorked
+					<< ", PTO Remaining: " << worker.PTORemaining << endl;
+			
+			}
+		}
+	}
+	void editWorkerInfo(const string& userID, const string& newName, const string& newPosition, const string& newHoursWorked, const string& newPTORemaining) {
+		auto it = workers.find(userID);
+		if (it != workers.end()) {
+			it->second.name = newName;
+			it->second.position = newPosition;
+			it->second.hoursWorked = newHoursWorked;
+			it->second.PTORemaining = newPTORemaining;
+			
+			saveWorkerData(filePath);
+			cout << "Worker information updated successfully." << endl;
+		}
+		else {
+			cout << "Error: Worker not found." << endl;
+		}
+	}
+		
+
+	
+};
+
+//Ez it azert van mert nem tudja ezt a karaktert valahogy a billentyuzetem kiirni.
+//<<
