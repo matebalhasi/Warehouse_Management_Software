f6daa1736e7d1814abd3f660c2738a11dd97cf71
Hindemith00 <91939821+Hindemith00@users.noreply.github.com>
2024-12-08 17:18:10 +0100

    Add files via upload

---
+++ Complaint.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include "Complaint.h"
+#include <list>
+
+int main()
+{
+	Complaint complaint;
+	std::list<Complaint>clista;
+	complaint.beolvas();
+	complaint.kiir();
+	complaint.add();
+
+}
