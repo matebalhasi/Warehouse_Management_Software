c60768cd90751caf2cc3fac295aa0945f60dcffa
Hindemith00 <91939821+Hindemith00@users.noreply.github.com>
2024-12-09 22:58:00 +0100

    Add files via upload

--- Complaint.h
+++ Complaint.h
@@ -10,29 +10,12 @@ public:
 	list<string>lista;
 	list<string>::iterator it;
 	int sorszam = 0;
-	/*
-	bool operator<(const Complaint& c) const {
-		if (it<c.it)
-			return false;
-		if (it <c.it)
-			return true;
-		return it< c.it;
-	}
 	
-	friend std::ostream& operator<<(std::ostream& s, const list<string>lista) {
-		for (auto it=lista.begin(); it < lista.end(); it++)
-		{
-			s << lista.it;
-		}
-		return s;
-	}
-	*/
-
 	void beolvas() {
 		std::ifstream input("complaints.txt");
 		string s;
 		if (input.is_open() == false) {
-			cout << "Could not open file" << endl;
+			cout << "Nem sikerult megnyitni az allomanyt" << endl;
 			exit(1);
 		}
 		
@@ -53,7 +36,7 @@ public:
 				
 		
 		input.close();
-		cout << "reading is complete " << endl;
+		cout << "beolvasas meg van" << endl;
 	}
 	 void kiir() {
 		 for (auto v : lista)
@@ -65,14 +48,14 @@ public:
 		 mycomplaint = '*';
 		 mycomplaint += to_string(++sorszam);
 		 mycomplaint += '.';
-		 cout << "What seems to be the issue?" << endl;
+		 cout << "Mi a panasza?" << endl;
 		 getline(cin, be);
 		 be += '\n';
 		 mycomplaint += be;
 		 lista.push_back(mycomplaint);
 		 ofstream of("complaints.txt");
 		 for (auto v : lista)
-			 of << v<< "\n";
+		of << v<< "\n";
 		 of.close();
 	 }
 		
